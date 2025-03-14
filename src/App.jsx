3a75f98aad662b80da2bee8c7ff19e6c3677ffd2
Ilyas Khisyametdinov <hopi@mail.ru>
2025-03-14 17:24:32 +0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import InventoryList from "./components/InventoryList"
 
 
 function App() {
 
   return (
     <>
-      <h1
-        className="text-3xl text-center text-red-700"
-      >Welcome to Vite with TailwindCSS and React</h1>
+      <InventoryList />
     </>
   )
 }
